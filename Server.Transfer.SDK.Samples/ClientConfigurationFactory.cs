cb28270ef8aa6222ae9a51e90a2f69a4e7d67d0a
Sameer Khan <sameer.khan@relativity.com>
2023-11-15 17:11:06 +0530

    Isolated and Modified assesmbly name, public packages and namespaces.

---
+++ Server.Transfer.SDK.Samples/ClientConfigurationFactory.cs
@@ -0,0 +1,53 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="ClientConfigurationFactory.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace Relativity.Server.Transfer.SDK.Samples
+{
+    using System.ComponentModel;
+	using Relativity.Transfer;
+	using Relativity.Transfer.Aspera;
+    using Relativity.Transfer.FileShare;
+    using Relativity.Server.Transfer.SDK.Samples.Enums;
+
+    public class ClientConfigurationFactory
+    {
+        public ClientConfiguration Create()
+        {
+            TransferMode transferMode = SampleRunner.GetTransferMode();
+
+            switch (transferMode)
+            {
+                case TransferMode.Aspera:
+                    return new AsperaClientConfiguration
+                    {
+                        // Common properties
+                        BadPathErrorsRetry = false,
+                        FileNotFoundErrorsRetry = false,
+                        MaxHttpRetryAttempts = 2,
+                        PreserveDates = true,
+                        TargetDataRateMbps = 5,
+
+                        // Aspera specific properties
+                        EncryptionCipher = "AES_256",
+                        OverwritePolicy = "ALWAYS",
+                        Policy = "FAIR",
+                    };
+                case TransferMode.Fileshare:
+                    return new FileShareClientConfiguration()
+                    {
+                        // Common properties
+                        BadPathErrorsRetry = false,
+                        FileNotFoundErrorsRetry = false,
+                        MaxHttpRetryAttempts = 2,
+                        PreserveDates = true,
+                        TargetDataRateMbps = 5,
+                    };
+                default:
+                    throw new InvalidEnumArgumentException("Specified TransferMode enum value is invalid");
+            }
+        }
+    }
+}
---
+++ Server.Transfer.SDK.Samples/Enums/TransferMode.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="TransferMode.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace Relativity.Server.Transfer.SDK.Samples.Enums
+{
+    public enum TransferMode
+    {
+        Aspera,
+        Fileshare
+    }
+}
